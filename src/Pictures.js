aa4bb3db11bf92824f6b3ab802929e61c26547aa
Jay.Lee <kkomarocker@gmail.com>
2018-06-18 13:15:16 -0400

    feat(client): Captured image shown for analyzation

--- src/Pictures.js
+++ src/Pictures.js
@@ -9,6 +9,7 @@ import {
 } from "semantic-ui-react";
 
 const Pictures = props => {
+  console.log(props);
   if (props.registered) {
     return (
       <Header
@@ -73,32 +74,59 @@ const Pictures = props => {
   return (
     <div>
       <div>
-        {props.logInStatus && props.data.FaceMatches[0].Similarity > 95 ? (
-          <Grid>
-            <Grid.Row>
-              <Grid.Column style={{ padding: "28px 0 0 10px" }}>
-                <Card
-                  centered={true}
-                  style={{ height: "410px", width: "480px" }}
-                >
-                  <Image
-                    alt={props.name}
-                    src={`https://s3.amazonaws.com/facerdb/${props.name.toLowerCase()}profile.jpg`}
+        {props.logInStatus && !props.data.UnmatchedFaces.length ? (
+          !props.data.UnmatchedFaces.length &&
+          props.data.FaceMatches[0].Similarity > 95 ? (
+            <Grid>
+              <Grid.Row>
+                <Grid.Column style={{ padding: "28px 0 0 10px" }}>
+                  <Card
                     centered={true}
-                    style={{ height: "320px", width: "240px" }}
-                  />
-                  <Card.Content>
-                    <Card.Header style={{ fontSize: "1.7em" }}>
-                      {props.name}
-                    </Card.Header>
-                    <Card.Description style={{ fontSize: "1.3em" }}>
-                      Student @ Fullstack Academy
-                    </Card.Description>
-                  </Card.Content>
-                </Card>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+                    style={{ height: "415px", width: "350px" }}
+                  >
+                    <Image
+                      alt={props.name}
+                      src={`https://s3.amazonaws.com/facerdb/${props.name.toLowerCase()}profile.jpg`}
+                      centered={true}
+                      style={{ height: "320px", width: "240px" }}
+                    />
+                    <Card.Content>
+                      <Card.Header style={{ fontSize: "1.7em" }}>
+                        {props.name}
+                      </Card.Header>
+                      <Card.Description style={{ fontSize: "1.3em" }}>
+                        Student @ Fullstack Academy
+                      </Card.Description>
+                      <Card.Description style={{ fontSize: "1.3em" }}>
+                        Similarity Score: {props.data.FaceMatches[0].Similarity}
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          ) : (
+            <div>
+              <Header
+                size="huge"
+                style={{
+                  fontSize: "3.5em",
+                  color: "white",
+                  padding: "28px 0 0 10px"
+                }}
+              >
+                User not Valid.<br />Please register
+              </Header>
+              <Container>
+                <Button
+                  size="massive"
+                  color="red"
+                  content="Register"
+                  onClick={() => props.register()}
+                />
+              </Container>
+            </div>
+          )
         ) : (
           <div>
             <Header
@@ -114,6 +142,7 @@ const Pictures = props => {
           </div>
         )}
       </div>
+
       <div>
         {props.detectedData.FaceDetails ? (
           <Grid divided="vertically">
@@ -128,76 +157,82 @@ const Pictures = props => {
                 content="Analyzed Result"
               />
             </Container>
-            <Grid.Row columns={4}>
-              <Grid.Column>
-                <Container>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header as="h1">
-                        Emotional Status:{" "}
-                        {props.detectedData.FaceDetails[0].Emotions[0].Type}
+            <Grid devided="vertically">
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <Container>
+                    <Card style={{ height: "200px", width: "260px" }}>
+                      <Image
+                        alt={props.name}
+                        src={props.pictures}
+                        centered={true}
+                        style={{ height: "180px", width: "260px" }}
+                      />
+                      <Card.Header style={{ fontSize: "1.7em" }}>
+                        Captured Image
                       </Card.Header>
-                    </Card.Content>
-                  </Card>
-                </Container>
-              </Grid.Column>
-              <Grid.Column>
-                <Container>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header as="h1">
-                        Has Eyeglasses:{" "}
-                        {props.detectedData.FaceDetails[0].Eyeglasses.Value
-                          ? "Yes"
-                          : "No"}
-                      </Card.Header>
-                    </Card.Content>
-                  </Card>
-                </Container>
-              </Grid.Column>
-              <Grid.Column>
-                <Container>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header as="h1">
-                        Age Range:
-                        {props.detectedData.FaceDetails[0].AgeRange.Low}-
-                        {props.detectedData.FaceDetails[0].AgeRange.High}
-                      </Card.Header>
-                    </Card.Content>
-                  </Card>
-                </Container>
-              </Grid.Column>
-              <Grid.Column>
-                <Container>
-                  <Card>
-                    <Card.Content>
-                      <Card.Header as="h1">
-                        Smiled:{" "}
-                        {props.detectedData.FaceDetails[0].Smile.Value
-                          ? "Yes"
-                          : "No"}
-                      </Card.Header>
-                    </Card.Content>
-                  </Card>
-                </Container>
-              </Grid.Column>
-            </Grid.Row>
+                    </Card>
+                  </Container>
+                </Grid.Column>
+                <Grid.Row columns={2}>
+                  <Grid.Column>
+                    <Container>
+                      <Card>
+                        <Card.Content>
+                          <Card.Header as="h1">
+                            Emotional Status:{" "}
+                            {props.detectedData.FaceDetails[0].Emotions[0].Type}
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </Container>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Container>
+                      <Card>
+                        <Card.Content>
+                          <Card.Header as="h1">
+                            Has Eyeglasses:{" "}
+                            {props.detectedData.FaceDetails[0].Eyeglasses.Value
+                              ? "Yes"
+                              : "No"}
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </Container>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Container>
+                      <Card>
+                        <Card.Content>
+                          <Card.Header as="h1">
+                            Age Range:{" "}
+                            {props.detectedData.FaceDetails[0].AgeRange.Low}-
+                            {props.detectedData.FaceDetails[0].AgeRange.High}
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </Container>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Container>
+                      <Card>
+                        <Card.Content>
+                          <Card.Header as="h1">
+                            Smiled:{" "}
+                            {props.detectedData.FaceDetails[0].Smile.Value
+                              ? "Yes"
+                              : "No"}
+                          </Card.Header>
+                        </Card.Content>
+                      </Card>
+                    </Container>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid.Row>
+            </Grid>
           </Grid>
-        ) : null
-        // <div>
-        //   <Header
-        //     size="huge"
-        //     style={{
-        //       fontSize: "3.5em",
-        //       color: "white",
-        //       padding: "28px 0 0 10px"
-        //     }}
-        //   >
-        //     Picture has not captured yet<br />Please take a snapshot!
-        //   </Header>
-        // </div>
-        }
+        ) : null}
       </div>
     </div>
   );
